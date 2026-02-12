f8c9b7a8a28f324c768e1d2ff22c0dc10980b15f
Ruvik <xalexmc@gmail.com>
2026-02-12 19:32:01 +0100

    Reapply "Refactor: Implementación de estructura POO con clase SSHClient y main interactivo"
    This reverts commit 4f5ad3077bf37ef35e59e0a81d975140ffd5b37d.

---
+++ src/main.py
@@ -0,0 +1,46 @@
+import os
+from ssh_manager import SSHClient
+
+# --- CONFIGURACIÓN ---
+# (En el futuro esto vendrá de un archivo seguro, por ahora hardcodeamos)
+HOST = "192.168.56.10"
+USER = "NOMBRE_USUARIO"      # <--- PON TU USUARIO
+PASS = "CONTRASEÑA"   # <--- PON TU CONTRASEÑA
+
+def main():
+    # 1. CREAR EL OBJETO (Instanciación)
+    # Aquí es donde "rellenamos el formulario".
+    # Creamos una variable 'mi_servidor' que ES una instancia de SSHClient.
+    print("🤖 Inicializando el Asistente SSH...")
+    mi_servidor = SSHClient(HOST, USER, PASS)
+
+    # 2. CONECTAR
+    # Le decimos a ESE objeto concreto que se conecte.
+    mi_servidor.conectar()
+
+    # Si la conexión falló, la propiedad .client será None. Verificamos:
+    if mi_servidor.client is None:
+        print("❌ No se pudo establecer conexión. Abortando.")
+        return
+
+    # 3. BUCLE DE COMANDOS (Interactividad)
+    # Como la conexión está abierta, podemos pedirle cosas repetidamente
+    while True:
+        comando = input("\n💻 Escribe un comando (Escribe 'salir' o 'exit' para cerrar ): ")
+        
+        if comando.lower() in ['salir', 'exit']:
+            break
+        
+        # Usamos el método de nuestro objeto para enviar la orden
+        respuesta = mi_servidor.ejecutar_comando(comando)
+        
+        print("--- RESPUESTA ---")
+        print(respuesta)
+        print("-----------------")
+
+    # 4. LIMPIEZA
+    mi_servidor.desconectar()
+    print("👋 ¡Hasta luego!")
+
+if __name__ == "__main__":
+    main()
